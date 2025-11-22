80e3f060865b0dc4291de8394af304674e90d9a7
ganeshshankar904 <GaneshShankar904@gmail.com>
2025-11-22 15:48:59 +0530

    Add files via upload

---
+++ Cricbuzz1/Cricbuzz1/Cricbuzz1App.swift
@@ -0,0 +1,17 @@
+//
+//  Cricbuzz1App.swift
+//  Cricbuzz1
+//
+//  Created by ganesh shankar on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Cricbuzz1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
